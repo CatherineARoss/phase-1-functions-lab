2e0bf7f38032e0bf1384d3002769270ee4941a83
Catherine Ross <fibervibes@gmail.com>
2023-03-21 11:48:16 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+  
+  function distanceFromHqInBlocks(blocks) {
+        return Math.abs(blocks - 42)
+  }
+
+  function distanceFromHqInFeet(blocks) {  
+        return Math.abs((blocks - 42) * 264)
+  }
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((start - destination) * 264)
+}
+
+function calculatesFarePrice(start, destination) {
+   let price = Math.abs((start - destination) * 264)
+    if (price <= 400){
+    return (price * 0);
+    } else if (price > 400 && price < 2000) {
+        return ((price - 400)* .02);
+    } else if (price > 2000 && price < 2500) {
+        return price = 25;
+    } else if (price >= 2500) {
+        return 'cannot travel that far'
+    }
+}
+
